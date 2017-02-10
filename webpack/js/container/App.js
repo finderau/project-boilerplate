2059b4cced3536bb47abbcba8cf5a5488fe9f0a5
Travis <noreply@travis-ci.org>
2017-02-10 16:25:56 +1100

    new-boilerplate

--- webpack/js/container/App.js
+++ webpack/js/container/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div className="new-project">New Project</div>;
+    return <div className="new-project">Hello World</div>;
   }
 }
 
